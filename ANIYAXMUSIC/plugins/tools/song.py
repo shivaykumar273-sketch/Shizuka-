61e3acd169ba7a48c2b0094d8aa9664cd3d9ab60
shivaykumar273-sketch <shivaykumar273@gmail.com>
2026-06-23 08:30:06 +0530

    Update song.py

--- ANIYAXMUSIC/plugins/tools/song.py
+++ ANIYAXMUSIC/plugins/tools/song.py
@@ -361,3 +361,4 @@ async def instagram_reel(client, message):
             await message.reply("Request was not successful.")
     else:
         await message.reply("Please provide a valid Instagram URL using the /reels command.")
+      
