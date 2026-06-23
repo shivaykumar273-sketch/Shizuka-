66b4af1d2c7d7a1386dfffea7f12fd7fcfa0b610
shivaykumar273-sketch <shivaykumar273@gmail.com>
2026-06-23 07:41:55 +0530

    Update whisper.py

--- ANIYAXMUSIC/plugins/tools/whisper.py
+++ ANIYAXMUSIC/plugins/tools/whisper.py
@@ -129,4 +129,5 @@ async def bot_inline(_, inline_query):
     else:
         answers = await _whisper(_, inline_query)
         await inline_query.answer(answers[-1], cache_time=0)
-                                               
+
+        
